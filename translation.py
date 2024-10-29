c368a490b2c5d01cd59d0e56505c3589f0f9fedc
P J Anthony <104015990+pjanthony2001@users.noreply.github.com>
2024-10-29 16:22:08 +0100

    Improve code quality of translation.py

--- translation.py
+++ translation.py
@@ -1,18 +1,30 @@
 import numpy as np
+from symmetries import generate_all_symmetries
 
-def translation(grid, pieces):
-    valid_positions = {}
+def translate_shape(grid, piece):
+    translations = []
     grid_rows, grid_cols = grid.shape
-
-    for piece_num, piece in pieces.items():
-        piece_rows, piece_cols = piece.shape
+    piece_rows, piece_cols = piece.shape    
+    for i in range(grid_rows - piece_rows + 1):
+        for j in range(grid_cols - piece_cols + 1):
+            window = grid[i:i + piece_rows, j:j + piece_cols]
+            if not np.any(window[piece]):  
+                new_grid = np.zeros(grid.shape)
+                new_grid[i:i + piece_rows, j:j + piece_cols] += piece
+                translations.append(new_grid)
+    return translations
+                
+def generate_translations(grid, sym_pieces):
+    valid_positions = {}
+    for piece_num, symmetries in sym_pieces.items():
         valid_positions[piece_num] = []
-        for i in range(grid_rows - piece_rows + 1):
-            for j in range(grid_cols - piece_cols + 1):
-                window = grid[i:i + piece_rows, j:j + piece_cols]
-                if np.any(window[piece]):  
-                    new_grid = np.copy(grid)
-                    new_grid[i:i + piece_rows, j:j + piece_cols] += piece
-                    valid_positions[piece_num].append(new_grid)
+        for piece in symmetries:
+            valid_positions[piece_num].append(translate_shape(grid, piece))
 
     return valid_positions
+
+grid = np.array([[1,0,0], [0,0,0], [0,0,0]])
+shapes = [np.array([[1, 1], [0, 1]])]
+all_sym = generate_all_symmetries(shapes)
+print(all_sym)
+[print(x) for x in translate_shape(grid, shapes[0])]
